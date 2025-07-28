440cd22569dbdf4a2e58fb2c594ce92766b5bd0a
Krishna Rohilla <rohillakrish2@gmail.com>
2025-07-28 11:44:59 +0530

    finalizing

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router";
 const Navbar = () => {
 
     return (
-        <nav className="navbar">
+        <nav className="navbar ">
             <Link to="/">
                 <p className= "text-2xl font-bold text-gradient">RESUMIND</p>
                 </Link>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
@@ -7,19 +7,19 @@ import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
     return [
-        { title: "Resumind" },
-        { name: "description", content: "Smart feedback for your dream job!" },
+        {title: "Resumind"},
+        {name: "description", content: "Smart feedback for your dream job!"},
     ];
 }
 
 export default function Home() {
-    const { auth, kv } = usePuterStore();
+    const {auth, kv} = usePuterStore();
     const navigate = useNavigate();
     const [resumes, setResumes] = useState<Resume[]>([]);
     const [loadingResumes, setLoadingResumes] = useState(false);
 
     useEffect(() => {
-        if(!auth.isAuthenticated) navigate('/auth?next=/');
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
     }, [auth.isAuthenticated])
 
     useEffect(() => {
@@ -40,27 +40,27 @@ export default function Home() {
     }, []);
 
     return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-        <Navbar />
+        <Navbar/>
 
         <section className="main-section">
             <div className="page-heading py-16">
                 <h1>Track Your Applications & Resume Ratings</h1>
                 {!loadingResumes && resumes?.length === 0 ? (
                     <h2>No resumes found. Upload your first resume to get feedback.</h2>
-                ): (
+                ) : (
                     <h2>Review your submissions and check AI-powered feedback.</h2>
                 )}
             </div>
             {loadingResumes && (
                 <div className="flex flex-col items-center justify-center">
-                    <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+                    <img src="/images/resume-scan-2.gif" className="w-[200px]"/>
                 </div>
             )}
 
             {!loadingResumes && resumes.length > 0 && (
                 <div className="resumes-section">
                     {resumes.map((resume) => (
-                        <ResumeCard key={resume.id} resume={resume} />
+                        <ResumeCard key={resume.id} resume={resume}/>
                     ))}
                 </div>
             )}
